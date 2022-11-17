72a1ff44ebf76f5925edb9c135e8891d7d9c9fc2
Douglas Oliveira <douglas.toliveira.dev@gmail.com>
2022-11-17 00:17:55 -0300

    Sales - findAll method

---
+++ backend/src/main/java/com/douglas/dsmeta/controllers/SaleController.java
@@ -0,0 +1,24 @@
+package com.douglas.dsmeta.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.douglas.dsmeta.entities.Sale;
+import com.douglas.dsmeta.services.SaleService;
+
+@RestController
+@RequestMapping(value = "/sales")
+public class SaleController {
+	
+	@Autowired
+	private SaleService service;
+	
+	@GetMapping
+	public List<Sale> findSales() {
+		return service.findSales();
+	}
+}
---
+++ backend/src/main/java/com/douglas/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,11 @@
+package com.douglas.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.douglas.dsmeta.entities.Sale;
+
+@Repository
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
