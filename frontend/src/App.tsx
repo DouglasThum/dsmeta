262e3f10a23eb537b8104bf034629642f80f71a6
Douglas Oliveira <douglas.toliveira.dev@gmail.com>
2022-11-18 15:38:32 -0300

    Toast - SMS Confirmation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,13 @@ import NotificationButtom from "./components/NotificationButtom";
 import Header from "./components/header";
 import SalesCard from "./components/SalesCard";
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
   return(
     <>
+      <ToastContainer />
       <Header />
       <main>
         <section id="sales">
